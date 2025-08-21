8fa9069ed1c498afe4f9f270770f6b5eb40c99f6
jon1 <gupa581333@gmail.com>
2025-08-21 09:04:54 -0300

    feat: otimization of diamondSimetryAlg

--- diamondSimetryAlg.js
+++ diamondSimetryAlg.js
@@ -1,3 +1,26 @@
+function drawSimetryDiamond(n) {
+  if (typeof n !== "number" || n === 0) return console.log("invalid input");
+  if (n === 1) return "*";
+  let result = "";
+  let leftBound = 0; // QUANDO INCREMENTADO CHEGARÁ AO VALOR DE RIGHBOUND
+  let rightBound = n - 1; // QUANDO DECREMENTADO CHEGARÁ AO VALOR DE LEFTBOUND
+  let pen = 0;
+  // n * n + n, O "+ n" SERÁ O NÚMERO SOBRESALENTE DE \n QUE SOMARÁ NA STRING
+  while (result.length < n * n + n) {
+    if (pen === n) {
+      result += "\n";
+      leftBound++;
+      rightBound--;
+      pen = 0;
+    } else {
+      const char = pen === leftBound || pen === rightBound ? "*" : " ";
+      result += char;
+      pen++;
+    }
+  }
+  return result;
+}
+
 //Input: 5
 
 // Output:
@@ -7,28 +30,4 @@
 //  * *
 // *   *
 
-function drawSimetryDiamond(n) {
-  if (typeof n !== "number" || n === 0) return console.log("invalid input");
-  if (n === 1) return "*";
-  let result = "";
-  let leftBound = 0;
-  let rightBound = n - 1;
-  let pen = 0;
-  while (result.length < n * n + n) {
-    if (pen === n) {
-      result += "\n";
-      leftBound++;
-      rightBound--;
-      pen = 0;
-    } else if (pen === leftBound || pen === rightBound) {
-      result += "*";
-      pen++;
-    } else {
-      result += " ";
-      pen++;
-    }
-  }
-  return result;
-}
-
-console.log(drawSimetryDiamond(5));
+console.log(drawSimetryDiamond(7));
